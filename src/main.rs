3c3ae20a38d8486ab82c32d28287206e8737d2c3
Sergei Belokon <srj.belokon@gmail.com>
2021-11-21 22:48:43 +0300

    improvement: add password validation checks

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,49 @@ use rand::Rng;
 use std::{process, str};
 
 fn main() {
-    let pswd: String = (0..arg_parse()
+    let mut pswd: String;
+    let parsed_option = arg_parse()
         .value_of("PSWD_LENGTH")
         .unwrap()
         .parse::<u8>()
         .unwrap_or_else(|e| {
-            eprintln!("{}", e);
+            eprintln!("error: {}", e);
             process::exit(1);
-        }))
-        .map(|_| rand::thread_rng().gen_range('!'..'~'))
-        .collect();
+        });
 
-    if arg_parse().occurrences_of("PSWD_LENGTH").eq(&0) {
-        eprintln!("warning: password length was not provided. using default value.")
+    // To satisfy all validation cariterias a password must be at least 4
+    // characters length, therefore the program will panic if provided
+    // `PSWD_LENGTH` value will be less then 4
+    if parsed_option < 4 {
+        eprintln!(
+            "error: to satisfy all security criterias password must be at least \
+            4 characters length"
+        );
+        process::exit(1);
     }
+
+    loop {
+        pswd = (0..parsed_option)
+            .map(|_| rand::thread_rng().gen_range('!'..='~'))
+            .collect();
+
+        // We have to ensure that `pswd` String contains at least one number,
+        // one sign, one lowercase and one uppercase character
+        if pswd.contains(rand::thread_rng().gen_range('0'..='9'))
+            && pswd.contains(rand::thread_rng().gen_range('a'..='z'))
+            && pswd.contains(rand::thread_rng().gen_range('A'..='Z'))
+            && (pswd.contains(rand::thread_rng().gen_range('!'..='/'))
+                || pswd.contains(rand::thread_rng().gen_range(':'..='@'))
+                || pswd.contains(rand::thread_rng().gen_range('['..='`'))
+                || pswd.contains(rand::thread_rng().gen_range('{'..='~')))
+        {
+            break;
+        }
+    }
+
+    if parsed_option.eq(&0) {
+        eprintln!("warning: password length was not provided. using default value.")
+    };
 
     println!("{}", pswd);
 }
@@ -38,7 +67,7 @@ fn arg_parse() -> ArgMatches<'static> {
                 .help("Password length"),
         )
         .after_help(
-            "Super trivial program that generates random passwrod.\n\
+            "Super trivial program that generates random password.\n\
         If `PSWD_LENGTH` option is not provided the programs uses default \
         lenght value, which is 12.",
         )
